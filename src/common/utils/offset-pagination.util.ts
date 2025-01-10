edd7c027d3185839d1aba283a49ef8508c0d4f9b
ihor voznyi <ihor.voznyi.dev@gmail.com>
2025-01-10 20:20:06 +0200

    feat: update pagination util with custom count query fn

--- src/common/utils/offset-pagination.util.ts
+++ src/common/utils/offset-pagination.util.ts
@@ -1,23 +1,42 @@
 import { SelectQueryBuilder } from 'typeorm';
 import { OffsetPaginationDto, PageOptionsDto } from '../dtos/offset-pagination';
 
+type PaginateOptions<T> = Partial<{
+  raw: boolean;
+  takeAll: boolean;
+  skipCount: boolean;
+  customCountQueryFn: SelectQueryBuilder<T>;
+}>;
+
+type PaginationResult<T> = [T[], OffsetPaginationDto];
+
 export async function paginate<T>(
-  builder: SelectQueryBuilder<T>,
+  queryBuilder: SelectQueryBuilder<T>,
   pageOptionsDto: PageOptionsDto,
-  options?: {
-    takeAll: boolean;
-    skipCount: boolean;
-  },
-): Promise<[T[], OffsetPaginationDto]> {
-  if (!options?.takeAll) {
-    builder.skip(pageOptionsDto.offset).take(pageOptionsDto.take);
+  options?: PaginateOptions<T>,
+): Promise<PaginationResult<T>> {
+  const {
+    raw = false,
+    takeAll = false,
+    skipCount = false,
+    customCountQueryFn: countQueryFn,
+  } = options || {};
+
+  if (!takeAll) {
+    queryBuilder.skip(pageOptionsDto.offset).take(pageOptionsDto.take);
   }
 
-  const entities = await builder.getMany();
+  const entities = raw
+    ? await queryBuilder.getRawMany()
+    : await queryBuilder.getMany();
 
   let count = -1;
-  if (!options?.skipCount) {
-    count = await builder.getCount();
+  if (!skipCount) {
+    count = await (countQueryFn
+      ? countQueryFn
+          .getRawOne()
+          .then((res) => parseInt(Object.values(res)[0] as string, 10))
+      : queryBuilder.getCount());
   }
 
   const meta = new OffsetPaginationDto(count, pageOptionsDto);
